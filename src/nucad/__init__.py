f99ae3a400f724e1aab3cc72a2489f6adb89d8e4
Takayuki Yano <yano.takayuki.55w@st.kyoto-u.ac.jp>
2025-09-09 16:47:04 +0900

    Enable to import SetupBase from nucad

--- src/nucad/__init__.py
+++ src/nucad/__init__.py
@@ -2,3 +2,5 @@ from . import setup
 from . import physics
 from . import core
 from .core import Track, Materials, pol2xyz, xyz2pol, pol, xyz
+from . import setup
+from .setup import SetupBase, make_metadata
